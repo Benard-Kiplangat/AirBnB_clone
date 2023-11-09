2dd07f83e249aa8ffa2ae62e74bc6b05a4d4d71e
benard Kiplangat <benardkiplangat97@gmail.com>
2023-11-09 15:12:02 +0300

    Updated the base class model to accept kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -9,14 +9,27 @@ class BaseModel(cmd.Cmd):
     """BaseModel class for the console of our AirBnB project
         attributes (class): Cmd module
     """
+    id = str(uuid.uuid4())
+    created_at = datetime.datetime.now().isoformat()
+    updated_at = datetime.datetime.now().isoformat()
+
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now().isoformat()
-        self.updated_at = datetime.datetime.now().isoformat()
-        if **kwargs is not None:
-            print(self.id)
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                if key != "__class__":
+                    setattr(self, key, value)
+                if "id" not in kwargs.keys():
+                    setattr(self, "id", str(uuid.uuid4()))
+                time = datetime.datetime.now().isoformat()
+                if "created_at" not in kwargs.keys():
+                    setattr(self, "created_at", time)
+                if "updated_at" not in kwargs.keys():
+                    setattr(self, "updated_at", time)
         else:
-            print(self.created_at)
+            self.id = str(uuid.uuid4())
+            self.created_at = updated_at = datetime.datetime.now().isoformat()
 
     def __str__(self):
         return "[{}] ({}) <{}>".format(self.name, self.id, self.__dict__)
@@ -25,4 +38,6 @@ class BaseModel(cmd.Cmd):
         self.updated_at = datetime.datetime.now().isoformat()
 
     def to_dict(self):
-        return dict(self.__dict__)
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        return my_dict
